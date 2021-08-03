41b9d0ae09b3d33c1b42894cd23a606006bd3819
z21016ht <z21016ht@aiit.co.jp>
2021-08-04 01:38:00 +0900

    動作確認シェルスクリプトを新規作成

---
+++ report5-test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+readonly ERROR_MSG="引数には正の整数値を入力してください"
+
+echo "テストケース1　2と4を入力"
+value=`./report5.sh 2 4`
+if [ $value -eq 2 ];
+then
+	echo "成功"
+else
+	echo "エラー"
+fi
+
+echo "テストケース2　引数が１つしかない"
+value=`./report5.sh 3`
+if [ $value == $ERROR_MSG ];
+then
+	echo "成功"
+else
+	echo "エラー"
+fi
+
+echo "テストケース3　自然数以外の文字を入力"
+value=`./report5.sh a b`
+if [ $value == $ERROR_MSG ];
+then
+	echo "成功"
+else
+	echo "エラー"
+fi
